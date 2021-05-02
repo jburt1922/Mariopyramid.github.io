3fa0003ddca644661744e9ac99bdcde9ef8a3f55
jburt19 <>
2021-05-02 00:49:05 +0000

    ERRORS AND HEIGHT

--- mario.js
+++ mario.js
@@ -1,59 +1,106 @@
 
-let button = document.querySelector("#button");
-button.addEventListener("click", () => {
-    determineHeightAndThenDrawPyramid();
-   
-});
-function determineHeightAndThenDrawPyramid(){
-  document.querySelector('#pyramid').innerHTML = "";
-  
-  printPyramid(document.querySelector('#height').value);
-}
-/* 
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
-function printPyramid(height) {
-    // height is the number of rows. For each row...
-    for (var row = 0; row < height; row++) {
-        // figure out how many bricks in this row (aka the '#' characters)
-        var numBricks = row + 2;
+var heightElem = document.getElementById("height");
+var formElem = document.getElementById("draw-form");
 
-        // use the information from the user
-        // figure out how many space characters
+// set a handler function for the form's submission event
+formElem.onsubmit = function(event) {
+
+    // QUIZ
+    // what happens if we don't do this?
+    event.preventDefault();
+
+    // QUIZ
+    // what happens if we don't do this?
+    function clearError(){
+      document.querySelector('.height').remove();
+    }
+
+    // figure out the height the user typed
+    heightStr = heightElem.value;
+
+    // TODO 1
+    // if they didn't type anything at all, give a different error message,
+    // something like "Please provide a height"
+
+
+    // convert the string to an int
+    height = parseInt(heightStr);
+
+    // if the height is not-a-number, yell at them and exit early
+    // TODO 2
+    // negative numbers and zero should also be rejected here
+    if((isNaN(height) == "")){
+      displayError("Please enter a Height");
+      } else {
+        displayError("That's not a valid height.");
+        return;
+    }
+
+    // if the height is absurdly tall, yell at them and exit early
+    var tooTall = 100;
+    if (height > tooTall) {
+        displayError("Are you cray? I can't build a pyramid that tall.");
+        return;
+    }
+
+    // draw pyramid with the specified height
+    drawPyramid(height);
+}
+
+
+/**
+ * displayError
+ *
+ * Displays an error message on the text input, and colors it red
+ */
+function displayError(message) {
+    heightElem.className = "invalid-field";
+    document.querySelector(".error-message").innerHTML = message;
+}
+
+
+/*
+ * clearError
+ *
+ * Undisplays the error message and removes the red CSS style
+ */
+function clearError(message) {
+    // TODO 3
+    // implement this function.
+}
+
+
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+function drawPyramid(height) {
+
+    // first, clear the old content
+    document.getElementById("pyramid").innerHTML = "";
+
+    // for each row....
+    for (var row = 0; row < height; row++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
         var numSpaces = height - row - 1;
 
-        // build up a string for this row. Starts empty...
-        var rowStr = ".";
-        
-        // add the spaces
+        // build up a string for this row
+        var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
-            rowStr += ".";    
+            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
         }
-
-        // add the bricks
         for (var i = 0; i < numBricks; i++) {
             rowStr += "#";
-        
         }
 
-        // print the string
-        console.log(rowStr);
-      
-    let para = document.createElement("p");
-      //console.log(rowStr);
-      let node = document.createTextNode(rowStr);
-      //console.log(node);
-      para.appendChild(node);
-      let element = document.getElementById("pyramid");
-      //console.log(document.getElementById("pumpkin"));
-      element.appendChild(para); 
+        // make a <p> element for this row, and insert it into the #pyramid container
+        rowElem = document.createElement("p");
+        rowElem.innerHTML = rowStr;
+        document.getElementById("pyramid").appendChild(rowElem);
     }
-  }
+}
